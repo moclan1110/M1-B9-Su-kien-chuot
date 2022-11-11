2eea4191f8f04249eeec2e4c9e8779e1cdd2567d
moclan110 <vulan11101997@gmail.com>
2022-11-11 08:00:16 +0700

    su kien chuot

---
+++ main.js
@@ -0,0 +1,21 @@
+let imgObj = null;
+imgObj = document.getElementById("myImage");
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+    imgObj.style.top = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+window.onload = init;
+
+
+
+
+
+
+
+
+
